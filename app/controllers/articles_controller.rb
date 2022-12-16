852d294fd0094a96b1004c2884c5607212e54376
Morgan Murrah <morgan@morganwebdev.com>
2022-12-16 12:27:03 -0800

    HTTP Basic Auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "name", password: "test", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
