31f24ee451f7ec1afb1cbd4721cd0daeaab77e68
Morgan Murrah <morgan@morganwebdev.com>
2022-12-15 23:00:16 -0800

    Steps taken to add public, private and archived states to controllers and views and models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
-    has_many :comments
-
+    include Visible
+  
+    has_many :comments, dependent: :destroy
+  
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
-end
+  end
   
---
+++ app/models/concerns/visible.rb
@@ -0,0 +1,24 @@
+module Visible
+    extend ActiveSupport::Concern
+  
+    VALID_STATUSES = ['public', 'private', 'archived']
+  
+    included do
+      validates :status, inclusion: { in: VALID_STATUSES }
+    end
+  
+    class_methods do
+      def public_count
+        where(status: 'public').count
+      end
+    end
+
+    def status_is
+      status
+    end  
+  
+    def archived?
+      status == 'archived'
+    end
+  end
+  
