7a38816d37e8e4f8c18c8ee8751ec4e15fc4eadd
Morgan Murrah <morgan@morganwebdev.com>
2022-12-30 00:03:36 -0800

    Make systems test pass, change selector of headline

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -4,7 +4,7 @@ class ArticlesTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit "/"
   
-    assert_selector "h1", text: "Articles"
+    assert_selector "button", text: "Celebrate ☕!"
     assert_selector "h4", text: "Loud articles:"
     assert_selector "h4", text: "subdued articles:"
   end
