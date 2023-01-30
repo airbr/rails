af7af6efb4f4b1e90bc9d72bbd26050938f03d34
Morgan Murrah <morgan@morganwebdev.com>
2023-01-29 22:17:41 -0800

    Remove Alpine JS, add Hero image

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails'
-import 'alpine-turbo-drive-adapter'
-import Alpine from 'alpinejs'
 
-window.Alpine = Alpine
-Alpine.start()
+console.log('Hello from app/javascript/application.js!');
